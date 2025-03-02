68656d95f47b2be8721f6abeaa0eafb992102d9b
wudiming <sxwudiming@163.com>
2025-03-02 11:48:34 +0800

    iptowwww
    upupup

--- src/common.rs
+++ src/common.rs
@@ -934,7 +934,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "http://140.238.16.119:21114".to_owned()
+    "https://rd.1128.pp.ua:21114".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
